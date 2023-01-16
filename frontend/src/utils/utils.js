a03e2b35840fbc10b0ae9f801dad92682119afac
rosenholmjohanna <rosenholm.johanna88@gmail.com>
2023-01-16 21:58:15 +0100

    change in url login

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,5 +1,7 @@
 
-const BASE_URL = "http://localhost:8080";
+// const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://final-project-fullstack-lsdubteuzq-uc.a.run.app";
+
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
 
